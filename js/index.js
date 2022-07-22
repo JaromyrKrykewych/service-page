24467507cb1593829d50a8305d6e2cf97b7aa911
Jaromyr Krykewych <krykewychjaromyr@gmail.com>
2022-07-22 13:11:27 -0300

    primeras funciones

--- js/index.js
+++ js/index.js
@@ -10,3 +10,41 @@ closeMenu.addEventListener('click', () => {
     menuMobile.classList.toggle('opened')
 });
 
+//Selector Function
+
+const selectorFunction = (targetID, boxesClass, targetStyle, boxStyle) => {
+    let divTargets = document.getElementById(targetID);
+    let targets = [...divTargets.children];
+    let boxes = document.getElementsByClassName(boxesClass);
+    let skills = [...boxes]
+    //console.log({targets, skills});
+
+    targets.forEach(target => {
+        target.addEventListener('click', e => {
+            let idx = targets.indexOf(e.target)
+            console.log(e.target, idx);
+            if (!e.target.classList.contains(targetStyle)) {
+                for (let i=0; i<targets.length; i++){
+                    if (targets[i].classList.contains(targetStyle)){
+                        targets[i].classList.remove(targetStyle)
+                        skills[i].classList.remove(boxStyle)
+                    }
+                }
+                e.target.classList.add(targetStyle)
+                skills[idx].classList.add(boxStyle)
+            }
+        })
+    })
+}
+
+//skills selector
+
+selectorFunction('skills-targets', 'skill-box', 'active', 'active')
+
+//Offer Selector
+
+selectorFunction('offer-targets', 'offer-box', 'show', 'active')
+
+//people says
+
+selectorFunction('testimony-targets', 'testimony-box', 'active', 'active')
